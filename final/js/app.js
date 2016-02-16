795f613376cd210d6744fcefd00ef3e3af44ca85
ppeverett <ppeverett@gmail.com>
2016-02-15 17:50:52 -0800

    cleaned up the js

--- final/js/app.js
+++ final/js/app.js
@@ -1,26 +1,30 @@
-// $('#hill').on('click', function() {
-//   $('.tweets').append('<a class="twitter-timeline" href="https://twitter.com/HillaryClinton" data-widget-id="699313292494241792">Tweets by @HillaryClinton</a>');
-// });
+// First Attempt - simple
+    // $('#hill').on('click', function() {
+    //   $('.tweets').append('<a class="twitter-timeline" href="https://twitter.com/HillaryClinton" data-widget-id="699313292494241792">Tweets by @HillaryClinton</a>');
+    //   $('.tweets').empty();
+    // });
 
-$('#bern').on('click', function(){
-  $('.twitter-timeline').attr({
-    "href" : "https://twitter.com/berniesanders",
-    "data-widget-id" : "698626932720562176"
-  });
-  $('.tweets').css('background', 'yellow')
-});
+// Second Attempt - using var
+    var ahref = '<a class="twitter-timeline" href="https://twitter.com/HillaryClinton" data-widget-id="699313292494241792">Tweets by @HillaryClinton</a>';
 
-// $('#hill').on('click', function(){
-//   $('.tweets').css('background','yellow');
-// });
-// $('#bern').on('click', function(){
-//   $('.tweets').css('background','green');
-// });
+    $('#hill').on('click', function() {
+      $('.tweets').add('ahref');
+      $('.tweets').empty();
+    });
 
-// var tweet = '<a class="twitter-timeline" href="https://twitter.com/berniesanders" data-widget-id="698626932720562176">Tweets by @berniesanders</a>'
-// var tW = document.createElement('a');
-// tW.innerHTML = tweet;
-// document.appendChild(tweet);
+// Test that jquery works on CSS
+    // $('#hill').on('click', function(){
+    //   $('.tweets').css('background','yellow');
+    // });
+    // $('#bern').on('click', function(){
+    //   $('.tweets').css('background','green');
+    // });
+
+// Use JS (not Jquery)
+    // var tweet = '<a class="twitter-timeline" href="https://twitter.com/berniesanders" data-widget-id="698626932720562176">Tweets by @berniesanders</a>'
+    // var tW = document.createElement('a');
+    // tW.innerHTML = tweet;
+    // document.appendChild(tweet);
 
 // var tweets = document.createElement('a');
 // var content = '<a class="twitter-timeline" href="https://twitter.com/berniesanders" data-widget-id="698626932720562176">Tweets by @berniesanders</a>'
