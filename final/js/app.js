292edfb5294de0a3fb307853b03d111f9ecc708f
ppeverett <ppeverett@gmail.com>
2016-02-15 20:50:57 -0800

    tweaks to jQuery

--- final/js/app.js
+++ final/js/app.js
@@ -5,11 +5,16 @@
     // });
 
 // Second Attempt - using var
-    var ahref = '<a class="twitter-timeline" href="https://twitter.com/HillaryClinton" data-widget-id="699313292494241792">Tweets by @HillaryClinton</a>';
+    // var ahref = '<a class="twitter-timeline" href="https://twitter.com/HillaryClinton" data-widget-id="699313292494241792">Tweets by @HillaryClinton</a>';
 
     $('#hill').on('click', function() {
-      $('.tweets').add('ahref');
-      $('.tweets').empty();
+      $('div.tweets').html("<a class='twitter-timeline' href='https://twitter.com/HillaryClinton' data-widget-id='699313292494241792'>Tweets by @HillaryClinton</a>");
+      // $('div.tweets').empty();
+    });
+
+    $('#bern').on('click', function() {
+      $('div.tweets').html("<a class='twitter-timeline' href='https://twitter.com/berniesanders' data-widget-id='698626932720562176'>Tweets by @BernieSanders</a>");
+      // $('div.tweets').empty();
     });
 
 // Test that jquery works on CSS
@@ -19,15 +24,3 @@
     // $('#bern').on('click', function(){
     //   $('.tweets').css('background','green');
     // });
-
-// Use JS (not Jquery)
-    // var tweet = '<a class="twitter-timeline" href="https://twitter.com/berniesanders" data-widget-id="698626932720562176">Tweets by @berniesanders</a>'
-    // var tW = document.createElement('a');
-    // tW.innerHTML = tweet;
-    // document.appendChild(tweet);
-
-// var tweets = document.createElement('a');
-// var content = '<a class="twitter-timeline" href="https://twitter.com/berniesanders" data-widget-id="698626932720562176">Tweets by @berniesanders</a>'
-// tweets.appendChild(content);
-// var position = document.getElementsByClassName('tweets');
-// position.appendChild(tweets);
